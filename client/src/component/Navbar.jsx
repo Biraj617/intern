a522067b6896ecf4c0cddf71cfd83cb72d230ab7
Biraj Phuyel <biraj.p617@gmail.com>
2026-07-13 22:37:22 +0545

    Add React project

---
+++ client/src/component/Navbar.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import "./Navbar.css";
+
+export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+        <div className="navbar-logo">
+          <a href="/">MedicalApp</a>
+        </div>
+        <div className={`navbar-menu ${isOpen ? "active" : ""}`}>
+          <ul className="navbar-list">
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/about">About</a>
+            </li>
+            <li>
+              <a href="/services">Services</a>
+            </li>
+            <li>
+              <a href="/contact">Contact</a>
+            </li>
+            <li>
+              <a href="/login" className="btn-login">
+                Login
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="hamburger" onClick={toggleMenu}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </div>
+    </nav>
+  );
+}
